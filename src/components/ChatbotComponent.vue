86e1f1fb0e8ba661d1f4ba5e1da2203eb7f6dc25
Yahlex <alexisremyh@gmail>
2023-03-22 10:03:54 +0100

    maj chatbotcomponent

---
+++ src/components/ChatbotComponent.vue
@@ -0,0 +1,225 @@
+<script>
+
+/** On importe des fonctions du fichier chatHelper pour pouvoir les utiliser dans le script */
+import { converse, getTime, } from '../helpers/chatHelper';
+/** On créer un objet ChatbotComponent pour exporter le composant avec ses données */
+export default {
+  name: 'ChatbotComponent',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      dialog: [{
+        msg: 'Bonjour ! Que puis-je faire pour vous ?',
+        user: 'bot',
+        time: ""
+      }],
+      inputUserU: "Bonjour",
+      inputUser1: "Homme",
+      inputUser2: "Femme",
+    }
+  },
+  methods: {
+    dialogUndefined() {
+      this.dialog = converse(this.inputUserU, this.dialog)
+    },
+    dialogHomme() {
+      this.dialog = converse(this.inputUser1, this.dialog)
+    },
+    dialogFemme() {
+      this.dialog = converse(this.inputUser2, this.dialog)
+    }
+}
+}
+</script>
+
+
+    <!-- Structure html du chatbot -->
+<template>
+  <h1>Bonjour c'est le ChatbotComponent</h1>
+ <div class="page-chatbot">
+    <h1>{{ msg }}</h1>
+    <div class="chatbot-boxplus">
+    <div class="chatbot-box">
+      <div id="button-chatbox">
+        <button type="button" class="button-open-chatbox"> <img class="chat-bar-icons-2"
+            src="../assets/images/bot_logo.png">
+          <p id="bot-text">Chat avec Sneak Me !</p>
+        </button>
+
+      </div>
+      <div class="window-chatbot">
+        <ul class="scrollbar">
+          <li v-for="message in dialog">
+            <div class="userBox">
+              <p :class="message.user == 'bot' ? 'bot' : 'user'">{{ message.msg }}<p id="time">12:34</p></p>
+            </div>
+          </li>
+        </ul>
+        <div class="button-zone">
+          <button @click="dialogUndefined()" class="userButton">Voir le catalogue</button>
+          <button @click="dialogUndefined()" class="userButton">FAQ</button>
+          <button @click="dialogUndefined()" class="userButton">Connexion</button>
+          <button @click="dialogUndefined()" class="userButton">Inscription</button>
+          
+          <!-- <button @click="dialogHomme()" class="userButton">Homme</button>
+          <button @click="dialogFemme()" class="userButton">Femme</button>
+          <button @click="dialogUndefined()" class="userButton">Basket Basses</button>
+          <button @click="dialogUndefined()" class="userButton">Basket Hautes</button>
+          <button @click="dialogUndefined()" class="userButton">Choisir une couleur</button>
+          <button @click="dialogUndefined()" class="userButton">Ne pas choisir de couleur </button>  -->
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+
+       <!-- <!- CSS du chatbot --->  
+ <style>
+.chatbot-boxplus {
+  position: absolute;
+  right: 50px;
+  bottom: 0px;
+}
+.chatbot-box {
+  position: relative;
+  animation: chatbot-window 0.1s 1 linear alternate running;
+}
+/* ------BOUTON D'OUVERTURE------ */
+.button-open-chatbox {
+  position: relative;
+  background: linear-gradient(90deg, rgba(35,95,255,1) 0%, rgba(45,161,245,1) 100%);
+  width: 550px;
+  height: 60px;
+  bottom: 0;
+  right: 0;
+  cursor: pointer;
+  border-radius: 20px 20px 0px 0px;
+  border: 3px solid white;
+  border-bottom: none;
+}
+#bot-text {
+  text-align: left;
+  vertical-align: middle;
+  padding: 15px;
+  margin: 0;
+  color: white;
+  font-size: 18px;
+}
+.button-open-chatbox img {
+  justify-content: space-evenly;
+  box-sizing: border-box;
+  width: 50px;
+  float: left;
+}
+/* ------ZONE DES MESSAGES------ */
+.window-chatbot {
+  background-color: white;
+  max-height: 700px;
+  width: 550px;
+  height: 700px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  bottom: 0px;
+  right: 0px;
+  scroll-behavior: smooth;
+}
+.scrollbar {
+  overflow: auto;
+  scrollbar-width: none;
+  height: max-content;
+  transition: max-height 0.2s ease-out;
+  height: 700px;
+}
+.window-chatbot ul li {
+  list-style-type: none;
+  text-align: left;
+  padding:0;
+  margin: 0;
+}
+/* ------ZONE DES BOUTONS------ */
+.button-zone {
+  height:auto;
+  background-color: #e7e7e7;
+  padding-bottom: 15px;
+  border-left: 3px solid white;
+  border-right: 3px solid white;
+}
+.userButton {   
+  width: 240px;  
+  box-shadow:inset 0px 1px 0px 0px #bbdaf7;     
+  background:linear-gradient(90deg, rgba(35,95,255,1) 0%, rgba(45,161,245,1) 100%);          
+  border-radius:6px;     
+  border:1px solid #84bbf3;     
+  cursor:pointer;     
+  color:#ffffff;         
+  font-size:15px;     
+  font-weight:bold;     
+  padding:20px 0 20px 0;
+  margin: 15px 15px 0 15px;
+} 
+  
+.userButton:hover {   
+  background: linear-gradient(90deg, rgb(25, 69, 190) 0%, rgb(33, 118, 179) 100%);  
+} 
+.userButton:active {     
+  position:relative;     
+  top:1px; 
+}
+/* ------MESSAGES USER/BOT------ */
+.user {
+  margin: 2px;
+  padding: 15px;
+  color: white;
+  background: linear-gradient(90deg, rgba(35,95,255,1) 0%, rgba(45,161,245,1) 100%);   
+  width: 75%;
+  float: right;
+  margin-right: 10px;
+  margin-left: 25%;
+  border-radius: 10px 10px 0px 10px;
+  max-width: max-content;
+  animation: floatup .5s forwards
+}
+.bot {
+  margin: 2px;
+  padding: 15px;
+  color: rgb(0, 0, 0);
+  background: #e0e0e0;
+  width: 75%;
+  float: left;
+  margin-left: -30px;
+  margin-right: 25%;
+  border-radius: 10px 10px 10px 0px;
+  animation: floatup .5s forwards
+}
+.bot #time {
+  font-size: 10px;
+  margin: 10px 0px 0px 0px;
+}
+.user #time {
+  font-size: 10px;
+  text-align: right;
+  margin: 10px 0px 0px 0px;
+}
+/* ------ANIMATIONS------ */
+@keyframes chatbot-window {
+  0% { top: 700px; }
+  90% { top : 700px; }
+	100% { top: 0px; }
+}
+@keyframes floatup {
+    from {
+        transform: translateY(15px);
+        opacity: .0;
+    }
+    to {
+        transform: translateY(0px);
+        opacity: 1;
+    }
+}
+
+</style>
---
+++ src/views/UsersView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import AllSneakers from '../components/AllSneakers.vue';
+</script>
+
+<template>
+  <h1>Ceci est la HomeView</h1>
+  <AllSneakers />
+</template>
