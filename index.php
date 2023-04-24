4a9ea53dd1194d7fbc7a7ef2b21d43570586b52d
FlorianDRAY <floriandray97@gmail.com>
2023-04-24 11:09:57 +0200

    Ajout cross origin

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 require './env.php';
 
 // Réception d'une requête HTTP
